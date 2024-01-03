87380df4a974323e0db28c82cc213dd13090d1cb
Matthew Kehrt <mkehrt@roseandsigil.net>
2024-01-02 22:27:28 -0500

    test draw moar

--- src/day_1.ts
+++ src/day_1.ts
@@ -1,6 +1,6 @@
 export function run_day_1(context: CanvasRenderingContext2D) {
     context.fillStyle = "blue";
     // context.ellipse(10, 10, 100, 100, 0, 0, 2*3.14151926);
-    context.fillRect(0,0,10,10);
+    context.fillRect(10,10,100,100);
     console.log("day 1 " + context);
 }
