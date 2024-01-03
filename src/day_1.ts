15803427b7bec65e081019263c9e2cd0585f7d7f
Matthew Kehrt <mkehrt@roseandsigil.net>
2024-01-02 22:16:24 -0500

    loggery

--- src/day_1.ts
+++ src/day_1.ts
@@ -1,5 +1,5 @@
 export function run_day_1(context: CanvasRenderingContext2D) {
     context.fillStyle = "blue";
     context.ellipse(10, 10, 100, 100, 0, 0, 2*3.14151926);
-    console.log("day1");
+    console.log("day 1 " + context);
 }
